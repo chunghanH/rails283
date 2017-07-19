b87e1f78ee27ee620b4dbc07e832af33a071f6cf
Tom's Git <yous421@gmail.com>
2017-07-19 12:19:35 +0800

    hw2-2

--- homework2.rb
+++ homework2.rb
@@ -3,29 +3,24 @@ class Player
   def initialize(name)
     @name = name
   end 
-
 end
 
 # 人類玩家的類別
 class Human < Player
-
   def get_gesture
-  	begin
-  	  puts "please choose one of the following: R / P / S"
+    begin
+      puts "please choose one of the following: R / P / S"
       input = gets.chomp.upcase
     end while !['R', 'P', 'S'].include?(input)
     input
   end
-
 end
 
 # 電腦的類別
 class Computer < Player
-
   def get_gesture
-  	input = ['R', 'P', 'S'].shuffle!.last.upcase
+    input = ['R', 'P', 'S'].shuffle!.last.upcase
   end
-
 end
 
 class RPS
@@ -39,6 +34,7 @@ class RPS
     end while continue? == 'Y'
     puts 'Hasta la Vista, Baby!' 
   end 
+	  
   def intro
     #印出開場畫面，告訴玩家遊戲規則
     puts "<<< The Game Rock, Paper, Scissors >>>"
@@ -54,13 +50,13 @@ class RPS
     regex = /RS|SP|PR/
     choice = "#{p1_input}#{p2_input}"
     puts "#{@p1.name}:#{p1_input} #{@p2.name}:#{p2_input}"
-	if choice =~ regex
-		puts "#{@p1.name} wins the game!!!" 
-	elsif choice.reverse =~ regex
-		puts "#{@p2.name} wins the game!!!"  
-	else
-		puts 'Draw...'  
-	end
+    if choice =~ regex
+      puts "#{@p1.name} wins the game!!!" 
+    elsif choice.reverse =~ regex
+      puts "#{@p2.name} wins the game!!!"  
+    else
+      puts 'Draw...'  
+    end
   end
 
   def get_player_gestures
